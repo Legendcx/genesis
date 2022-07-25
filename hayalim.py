51c54b7f22ffdab3d52701e14470160caa89a4f6
burhan yiğiter <burhan.yigiter@gmail.com>
2022-07-25 15:56:30 +0300

    F stringle ilgili çalışma yapıldı.

--- hayalim.py
+++ hayalim.py
@@ -11,4 +11,9 @@ b = 4
 print(a + b)
 
 print("burhan" * 4)
+a = "Burhan"
+b = "Ebru"
+c = "May & Kyk"
 
+print(f"{a} ve {b}'nun çocukları olan {c} okula gidiyorlar.")
+## Burada ben f string metoduna kullandım. herkese başarılar.
